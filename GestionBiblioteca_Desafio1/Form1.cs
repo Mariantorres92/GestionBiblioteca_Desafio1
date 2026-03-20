69783327af477aac68d9f5caff276cccb90594f8
Mariantorres92 <tmariana814@gmail.com>
2026-03-20 13:47:25 -0600

    agregar pantalla de bienvenida con resumen de datos

--- GestionBiblioteca_Desafio1/Form1.cs
+++ GestionBiblioteca_Desafio1/Form1.cs
@@ -13,6 +13,7 @@ namespace GestionBiblioteca_Desafio1
         {
             InitializeComponent();
             ConfigurarFormulario();
+            MostrarBienvenida();
         }
 
         private void ConfigurarFormulario()
@@ -32,6 +33,75 @@ namespace GestionBiblioteca_Desafio1
             btnPrestamos.MouseLeave += (s, e) => ((Button)s).BackColor = Color.FromArgb(30, 58, 95);
             btnEstadisticas.MouseEnter += (s, e) => ((Button)s).BackColor = Color.FromArgb(52, 90, 138);
             btnEstadisticas.MouseLeave += (s, e) => ((Button)s).BackColor = Color.FromArgb(30, 58, 95);
+        }
+
+        private void MostrarBienvenida()
+        {
+            LimpiarPanel();
+
+            var lblBienvenida = new Label
+            {
+                Text = "Sistema de Gestion de Biblioteca",
+                Font = new Font("Segoe UI", 20f, FontStyle.Bold),
+                ForeColor = Color.FromArgb(30, 58, 95),
+                AutoSize = true,
+                Location = new Point(30, 40)
+            };
+
+            var lblSubtitulo = new Label
+            {
+                Text = "Selecciona una opcion del menu para comenzar.",
+                Font = new Font("Segoe UI", 12f),
+                ForeColor = Color.FromArgb(100, 100, 100),
+                AutoSize = true,
+                Location = new Point(30, 90)
+            };
+
+            var panelStats = new Panel
+            {
+                Location = new Point(30, 150),
+                Size = new Size(700, 100),
+                BackColor = Color.Transparent
+            };
+
+            int totalLibros = 0, totalUsuarios = 0, prestActivos = 0;
+            foreach (var m in _servicio.ObtenerMateriales()) totalLibros++;
+            foreach (var u in _servicio.ObtenerUsuarios()) totalUsuarios++;
+            foreach (var p in _servicio.ObtenerPrestamos(true)) prestActivos++;
+
+            panelStats.Controls.Add(CrearTarjetaInfo("Libros", totalLibros.ToString(), 0, Color.FromArgb(30, 58, 95)));
+            panelStats.Controls.Add(CrearTarjetaInfo("Usuarios", totalUsuarios.ToString(), 1, Color.FromArgb(34, 139, 34)));
+            panelStats.Controls.Add(CrearTarjetaInfo("Prestamos Activos", prestActivos.ToString(), 2, Color.FromArgb(200, 120, 0)));
+
+            panelContenido.Controls.AddRange(new Control[]
+                { lblBienvenida, lblSubtitulo, panelStats });
+        }
+
+        private Panel CrearTarjetaInfo(string titulo, string valor, int posicion, Color color)
+        {
+            var panel = new Panel
+            {
+                Location = new Point(posicion * 200, 0),
+                Size = new Size(180, 80),
+                BackColor = color
+            };
+            panel.Controls.Add(new Label
+            {
+                Text = titulo,
+                Font = new Font("Segoe UI", 9f),
+                ForeColor = Color.White,
+                Location = new Point(10, 10),
+                AutoSize = true
+            });
+            panel.Controls.Add(new Label
+            {
+                Text = valor,
+                Font = new Font("Segoe UI", 22f, FontStyle.Bold),
+                ForeColor = Color.White,
+                Location = new Point(10, 30),
+                AutoSize = true
+            });
+            return panel;
         }
 
         private void LimpiarPanel()
