eb3e3e605c0c89a3b42805bf3e5b25aa14bd97a7
Mariantorres92 <tmariana814@gmail.com>
2026-03-20 12:14:04 -0600

    feat: formulario principal con navegacion por modulos

--- GestionBiblioteca_Desafio1/Form1.Designer.cs
+++ GestionBiblioteca_Desafio1/Form1.Designer.cs
@@ -28,13 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnLibros = new System.Windows.Forms.Button();
+            this.btnUsuarios = new System.Windows.Forms.Button();
+            this.btnPrestamos = new System.Windows.Forms.Button();
+            this.btnEstadisticas = new System.Windows.Forms.Button();
+            this.panelContenido = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.Navy;
+            this.panelMenu.Controls.Add(this.btnEstadisticas);
+            this.panelMenu.Controls.Add(this.btnPrestamos);
+            this.panelMenu.Controls.Add(this.btnUsuarios);
+            this.panelMenu.Controls.Add(this.btnLibros);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(200, 450);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // btnLibros
+            // 
+            this.btnLibros.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLibros.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLibros.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLibros.ForeColor = System.Drawing.Color.White;
+            this.btnLibros.Location = new System.Drawing.Point(0, 0);
+            this.btnLibros.Name = "btnLibros";
+            this.btnLibros.Size = new System.Drawing.Size(200, 77);
+            this.btnLibros.TabIndex = 0;
+            this.btnLibros.Text = "📚 Libros";
+            this.btnLibros.UseVisualStyleBackColor = true;
+            this.btnLibros.Click += new System.EventHandler(this.btnLibros_Click);
+            // 
+            // btnUsuarios
+            // 
+            this.btnUsuarios.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnUsuarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUsuarios.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsuarios.ForeColor = System.Drawing.Color.White;
+            this.btnUsuarios.Location = new System.Drawing.Point(0, 77);
+            this.btnUsuarios.Name = "btnUsuarios";
+            this.btnUsuarios.Size = new System.Drawing.Size(200, 74);
+            this.btnUsuarios.TabIndex = 1;
+            this.btnUsuarios.Text = "👤 Usuarios";
+            this.btnUsuarios.UseVisualStyleBackColor = true;
+            // 
+            // btnPrestamos
+            // 
+            this.btnPrestamos.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPrestamos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrestamos.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrestamos.ForeColor = System.Drawing.Color.White;
+            this.btnPrestamos.Location = new System.Drawing.Point(0, 151);
+            this.btnPrestamos.Name = "btnPrestamos";
+            this.btnPrestamos.Size = new System.Drawing.Size(200, 70);
+            this.btnPrestamos.TabIndex = 2;
+            this.btnPrestamos.Text = "📋 Préstamos";
+            this.btnPrestamos.UseVisualStyleBackColor = true;
+            // 
+            // btnEstadisticas
+            // 
+            this.btnEstadisticas.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEstadisticas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEstadisticas.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEstadisticas.ForeColor = System.Drawing.Color.White;
+            this.btnEstadisticas.Location = new System.Drawing.Point(0, 221);
+            this.btnEstadisticas.Name = "btnEstadisticas";
+            this.btnEstadisticas.Size = new System.Drawing.Size(200, 68);
+            this.btnEstadisticas.TabIndex = 3;
+            this.btnEstadisticas.Text = "📊 Estadísticas";
+            this.btnEstadisticas.UseVisualStyleBackColor = true;
+            // 
+            // panelContenido
+            // 
+            this.panelContenido.BackColor = System.Drawing.Color.White;
+            this.panelContenido.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContenido.Location = new System.Drawing.Point(200, 0);
+            this.panelContenido.Name = "panelContenido";
+            this.panelContenido.Size = new System.Drawing.Size(600, 450);
+            this.panelContenido.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.panelContenido);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "Form1";
+            this.Text = "Sistema de Gestión de Biblioteca";
+            this.panelMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnLibros;
+        private System.Windows.Forms.Button btnUsuarios;
+        private System.Windows.Forms.Button btnEstadisticas;
+        private System.Windows.Forms.Button btnPrestamos;
+        private System.Windows.Forms.Panel panelContenido;
     }
 }
 
--- GestionBiblioteca_Desafio1/Form1.cs
+++ GestionBiblioteca_Desafio1/Form1.cs
@@ -1,20 +1,98 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using GestionBiblioteca_Desafio1.Services;
 
 namespace GestionBiblioteca_Desafio1
 {
     public partial class Form1 : Form
     {
+        private readonly BibliotecaService _servicio = new BibliotecaService();
+
         public Form1()
         {
             InitializeComponent();
+            ConfigurarFormulario();
+        }
+
+        private void ConfigurarFormulario()
+        {
+            panelMenu.BackColor = Color.FromArgb(30, 58, 95);
+
+            btnLibros.BackColor = Color.FromArgb(30, 58, 95);
+            btnUsuarios.BackColor = Color.FromArgb(30, 58, 95);
+            btnPrestamos.BackColor = Color.FromArgb(30, 58, 95);
+            btnEstadisticas.BackColor = Color.FromArgb(30, 58, 95);
+
+            btnLibros.MouseEnter += (s, e) => ((Button)s).BackColor = Color.FromArgb(52, 90, 138);
+            btnLibros.MouseLeave += (s, e) => ((Button)s).BackColor = Color.FromArgb(30, 58, 95);
+            btnUsuarios.MouseEnter += (s, e) => ((Button)s).BackColor = Color.FromArgb(52, 90, 138);
+            btnUsuarios.MouseLeave += (s, e) => ((Button)s).BackColor = Color.FromArgb(30, 58, 95);
+            btnPrestamos.MouseEnter += (s, e) => ((Button)s).BackColor = Color.FromArgb(52, 90, 138);
+            btnPrestamos.MouseLeave += (s, e) => ((Button)s).BackColor = Color.FromArgb(30, 58, 95);
+            btnEstadisticas.MouseEnter += (s, e) => ((Button)s).BackColor = Color.FromArgb(52, 90, 138);
+            btnEstadisticas.MouseLeave += (s, e) => ((Button)s).BackColor = Color.FromArgb(30, 58, 95);
+        }
+
+        private void LimpiarPanel()
+        {
+            panelContenido.Controls.Clear();
+        }
+
+        private void btnLibros_Click(object sender, EventArgs e)
+        {
+            LimpiarPanel();
+            var lbl = new Label
+            {
+                Text = "📚 Módulo de Libros - Próximamente",
+                Font = new Font("Segoe UI", 14f),
+                ForeColor = Color.FromArgb(30, 58, 95),
+                AutoSize = true,
+                Location = new Point(20, 20)
+            };
+            panelContenido.Controls.Add(lbl);
+        }
+
+        private void btnUsuarios_Click(object sender, EventArgs e)
+        {
+            LimpiarPanel();
+            var lbl = new Label
+            {
+                Text = "👤 Módulo de Usuarios - Próximamente",
+                Font = new Font("Segoe UI", 14f),
+                ForeColor = Color.FromArgb(30, 58, 95),
+                AutoSize = true,
+                Location = new Point(20, 20)
+            };
+            panelContenido.Controls.Add(lbl);
+        }
+
+        private void btnPrestamos_Click(object sender, EventArgs e)
+        {
+            LimpiarPanel();
+            var lbl = new Label
+            {
+                Text = "📋 Módulo de Préstamos - Próximamente",
+                Font = new Font("Segoe UI", 14f),
+                ForeColor = Color.FromArgb(30, 58, 95),
+                AutoSize = true,
+                Location = new Point(20, 20)
+            };
+            panelContenido.Controls.Add(lbl);
+        }
+
+        private void btnEstadisticas_Click(object sender, EventArgs e)
+        {
+            LimpiarPanel();
+            var lbl = new Label
+            {
+                Text = "📊 Módulo de Estadísticas - Próximamente",
+                Font = new Font("Segoe UI", 14f),
+                ForeColor = Color.FromArgb(30, 58, 95),
+                AutoSize = true,
+                Location = new Point(20, 20)
+            };
+            panelContenido.Controls.Add(lbl);
         }
     }
 }
